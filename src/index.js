8a0b4c47d725605da695fad7d1a4a90c88c59e4e
quantroc <ducquannguyen28699@gmail.com>
2022-07-22 10:06:08 +0700

    refactor code

--- src/index.js
+++ src/index.js
@@ -5,7 +5,14 @@ const handlebars = require('express-handlebars');
 const app = express();
 const port = 3000;
 
+const route = require('./routes');
+
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(express.urlencoded({
+  extended: true
+}));
+app.use(express.json());
 
 //http logger
 app.use(morgan('combined'));
@@ -17,20 +24,27 @@ app.engine('hbs', handlebars.engine({
 }));
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'resources/views'));
-console.log(path.join(__dirname, 'resources/views'));
+// console.log(path.join(__dirname, 'resources/views'));
 
+//ROUTE
+route(app);
 
-app.get('/', (req, res) => {
-  res.render('home');
-});
+// app.get('/', (req, res) => {
+//   res.render('home');
+// });
 
-app.get('/news', (req, res) => {
-  res.render('news');
-});
+// app.get('/news', (req, res) => {
+//   res.render('news');
+// });
 
-app.get('/search', (req, res) => {
-  res.render('search');
-});
+// app.get('/search', (req, res) => {
+//   res.render('search');
+// });
+
+// app.post('/search', (req, res) => {
+//   console.log(req.body);
+//   res.send('');
+// });
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
